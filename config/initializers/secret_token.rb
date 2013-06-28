2c7dd9e0dbe9a82bb89bea193146776298d5d1f7
Karol Kowalczyk <kowalczyk90@gmail.com>
2013-06-28 17:35:52 +0200

    pusta app rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sezon::Application.config.secret_token = '3fb03ee5d357dcd3a15b39cc7a0634a964428efee7200847dafcd9ddd3fa7cae6c8b4d6711bb5782804cae4b3f748186ece6bc24796cc15207f5aded9c3eb175'
